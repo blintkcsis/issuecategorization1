9b11a9e78d2edf026155d1710fb467e698896943
Balint Kocsis <balintkocsis01@gmail.com>
2024-08-27 18:27:58 +0100

    Initial commit

---
+++ nice.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import requests
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+def main():
+    
+
+    # Load environment variables from .env file
+    
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    client = OpenAI(api_key=api_key)
+    st.title("Bee wise kategorizálás")
+
+    # Text input
+    user_input = st.text_area("Hiba üzenet:", height=200)
+
+    # Submit button
+    if st.button("Küldés"):
+        if user_input:
+            try:
+                completion = client.chat.completions.create(
+                    model="ft:gpt-4o-2024-08-06:ef-hackathon-london-5::A0tGlKOA",
+                    messages=[
+                        {"role": "system", "content": "A feladata hogy eldöntse a hibabejelentés alapján a kategóriát és bejelentési osztályt."},
+                        {"role": "user", "content": user_input}
+                    ]
+                )
+                response = completion.choices[0].message
+                
+                # Display the response
+                st.success("Submission successful!")
+                st.subheader("Response:")
+                st.write(response.content)
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+        else:
+            st.warning("Nincs hibaüzenet hozzáadva.")
+
+if __name__ == "__main__":
+    main()
