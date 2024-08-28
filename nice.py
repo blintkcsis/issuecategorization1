4961339cf8e5250952c055e3473c0f6f78b266cc
Balint Kocsis <balintkocsis01@gmail.com>
2024-08-28 13:44:49 +0100

    New models added

--- nice.py
+++ nice.py
@@ -5,14 +5,19 @@ import os
 from dotenv import load_dotenv
 
 def main():
-    
-
     # Load environment variables from .env file
-    
     load_dotenv()
     api_key = os.getenv('API_KEY')
     client = OpenAI(api_key=api_key)
     st.title("Bee wise kategorizálás")
+
+    # Dropdown for model selection
+    model_options = {
+        "Legjobb":"ft:gpt-4o-2024-08-06:ef-hackathon-london-5::A1BaEBWI",
+        "Kicsi":"ft:gpt-4o-mini-2024-07-18:ef-hackathon-london-5::A1BLqEIV" ,
+        "Régi":"ft:gpt-4o-2024-08-06:ef-hackathon-london-5::A0tGlKOA"
+    }
+    selected_model = st.selectbox("Válassz kategorizálót:", model_options)
 
     # Text input
     user_input = st.text_area("Hiba üzenet:", height=200)
@@ -22,11 +27,11 @@ def main():
         if user_input:
             try:
                 completion = client.chat.completions.create(
-                    model="ft:gpt-4o-2024-08-06:ef-hackathon-london-5::A0tGlKOA",
+                    model=selected_model,
                     messages=[
                         {"role": "system", "content": "A feladata hogy eldöntse a hibabejelentés alapján a kategóriát és bejelentési osztályt."},
                         {"role": "user", "content": user_input}
-                    ]
+                    ],
                 )
                 response = completion.choices[0].message
                 
